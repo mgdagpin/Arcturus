29edda029cef0c2c73ef7988e2afcb2989b1222b
Marcelius Dagpin <mgdagpin@outlook.com>
2020-04-06 17:19:22 +0800

    Clean by code style

--- Src/Canary.Infrastructure/Persistence/Configurations/UserConfiguration.cs
+++ Src/Canary.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -1,9 +1,6 @@
 ﻿using Canary.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Canary.Infrastructure.Persistence.Configurations
 {
